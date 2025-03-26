8202931a6e0087d1760fd6cde6b8b27b358a9063
hackiku <ivan@pipewriter.io>
2025-03-26 19:11:02 +0100

    create t3 app

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+	schema: "./src/server/db/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: env.DATABASE_URL,
+	},
+	tablesFilter: ["metro-map_*"],
+} satisfies Config;
